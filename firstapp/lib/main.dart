020511286ec4989040be9581c3d0f56532e074bc
Hiraku Ohno <hiraku.ohno@glucose.jp>
2016-10-30 19:50:50 +0900

    Run official sample.

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,84 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new MaterialApp(
+    title: 'My app',
+    home: new MyScaffold()
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting
-        // the app, try changing the primarySwatch below to Colors.green
-        // and press "r" in the console where you ran "flutter run".
-        // We call this a "hot reload". Notice that the counter didn't
-        // reset back to zero -- the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo'),
-    );
+    return new Material(
+        child: new Column(
+            children: <Widget>[
+              new MyAppBar(
+                  title: new Text('Example title', style: Typography.white.title),
+                  ),
+              new Flexible(
+                  child: new Center(
+                      child: new Text('Hello, world!'),
+                      ),
+                  )
+            ],
+            ),
+        );
   }
 }
+class MyAppBar extends StatelessWidget {
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyAppBar({this.title});
 
-  // This widget is the home page of your application. It is stateful,
-  // meaning that it has a State object (defined below) that contains
-  // fields that affect how it looks.
-
-  // This class is the configuration for the state. It holds the
-  // values (in this case the title) provided by the parent (in this
-  // case the App widget) and used by the build method of the State.
-  // Fields in a Widget subclass are always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that
-      // something has changed in this State, which causes it to rerun
-      // the build method below so that the display can reflect the
-      // updated values. If we changed _counter without calling
-      // setState(), then the build method would not be called again,
-      // and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final Widget title;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that
-        // was created by the App.build method, and use it to set
-        // our appbar title.
-        title: new Text(config.title),
-      ),
-      body: new Center(
-        child: new Text(
-          'Button tapped $_counter time${ _counter == 1 ? '' : 's' }.',
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma tells the Dart formatter to use
-      // a style that looks nicer for build methods.
+    return new Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: new BoxDecoration(backgroundColor: Colors.blue[500]),
+      child: new Row(
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          new Flexible(
+              child: title,
+          ),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      )
     );
   }
 }
