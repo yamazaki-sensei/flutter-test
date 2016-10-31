d7ff31114a480ebad241b5f0d48319e28b0bf68f
Hiraku Ohno <hiraku.ohno@glucose.jp>
2016-11-01 01:03:03 +0900

    Temp commit

---
+++ firstapp/lib/client/qiita_api_client.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+
+class QiitaClient {
+  void get() {
+    var client = new HttpClient();
+    client.getUrl(Uri.parse("http://www.google.co.jp")).then((HttpClientRequest request) {
+      return request.close();
+    }).then((HttpClientResponse response) {
+      print(response);
+    });
+  }
+}
